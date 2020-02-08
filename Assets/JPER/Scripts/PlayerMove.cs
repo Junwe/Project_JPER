15a61fc3ff95458fdfcbb0b5de2c1e26d4c75b31
sickhye0929 <chjh0929@gmail.com>
2020-02-08 15:35:18 +0900

    플레이어 점프 기능 수정 중.

--- Assets/JPER/Scripts/PlayerMove.cs
+++ Assets/JPER/Scripts/PlayerMove.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public enum DirectionType
+    {
+        Left,
+        Right
+    }
+    public enum PlayerMoveType
+    {
+
+        MoveInGround,
+        JumppingUp,
+        FallingDown,
+        KnockBack
+    }
+
     public float MoveSpeed;
+    [Tooltip("Not use.")]
     public float JumpPower;
-    public Rigidbody2D myRigidbody;
+    public float JumpHeight;
+    //public Rigidbody2D myRigidbody;
 
     public JoyStickButton leftMoveBtn;
     public JoyStickButton RightMoveBtn;
-    // Start is called before the first frame update
+
+    private DirectionType currentDirection = DirectionType.Right;
+
     void Start()
     {
         leftMoveBtn.SetMoveEvent(LeftMove);
         RightMoveBtn.SetMoveEvent(RightMove);
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // 플레이어 방향 설정.
+        if (currentDirection == DirectionType.Left)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        else if (currentDirection == DirectionType.Right)
+            transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     public void LeftMove()
     {
         transform.localPosition += Vector3.left * MoveSpeed * Time.deltaTime;
-
         transform.localScale = new Vector3(-1f, 1f, 1f);
     }
 
@@ -39,8 +55,24 @@ public class PlayerMove : MonoBehaviour
 
     public void OnJump()
     {
-        myRigidbody.AddForce(Vector2.up * JumpPower);
+        //myRigidbody.AddForce(Vector2.up * JumpPower);
+
     }
 
+    private IEnumerator MoveToAir()
+    {
+        var playerPosition = transform.localPosition;
 
+        var moveDirection = Vector3.zero;
+        if (currentDirection == DirectionType.Left)
+            moveDirection = Vector3.left;
+        else if (currentDirection == DirectionType.Right)
+            moveDirection = Vector3.right;
+
+        while (playerPosition.y < JumpHeight)
+        {
+            
+            yield return null;
+        }
+    }
 }
