d7057e5af914880e599aba6838fa1c76641fccb4
sickhye0929 <chjh0929@gmail.com>
2020-03-06 02:47:56 +0900

    맵생성기 추가. 맵 json 데이터 및 데이터 저장 폴더 추가.

---
+++ Assets/JPER/Scripts/MapCreater.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MapCreater : MonoBehaviour
+{
+    private const string JSON_PATH = "MapData";
+    private const float GROUND_HEIGHT = .98f;
+    private const float GROUND_WIDTH = .98f;
+
+    [SerializeField]
+    private string jsonFileName;
+
+    [Header("Temporal Setting")]
+    [SerializeField]
+    private List<GameObject> groundPrefabs = null;
+
+    public int[,] map;
+
+    private void Start()
+    {
+        string jsonString = string.Empty;
+
+        using (StreamReader reader = new StreamReader(Path.Combine(Application.streamingAssetsPath, JSON_PATH, jsonFileName)+".json"))
+            jsonString = reader.ReadToEnd();
+
+        if (string.IsNullOrEmpty(jsonString) == true)
+        {
+            Debug.LogError("MapCreater.Start() : JSON string is empty.");
+            return;
+        }
+
+        StageData stageData = JsonUtility.FromJson<StageData>(jsonString);
+
+        for (int i = 0; i < stageData.map.GetLength(0); ++i)
+        {
+            for (int j = 0; j < stageData.map.GetLength(1); ++j)
+            {
+                if (map[i, j] == 0)
+                    continue;
+
+                float posX = GROUND_WIDTH * j;
+                float posY = GROUND_HEIGHT * i;
+                var temp = Instantiate(groundPrefabs[0], new Vector2(posX, posY), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/JPER/Scripts/StageData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class StageData
+{
+    //public List<string> groundNames;
+    public int[,] map;
+}
