cd37f2d2860abbc9c3f846be555a455c200a63ce
Junwe <junwe99@gmail.com>
2020-03-11 18:08:04 +0900

    맵 생성툴 작업중

---
+++ Assets/Editor/MapGridEditor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Grid))]
+public class MapGridEditor : Editor
+{
+    Grid grid;
+
+    private List<GameObject> _objList = new List<GameObject>();
+
+    void OnEanble()
+    {
+    }
+    void OnSceneGUI()
+    {
+        int crtID = GUIUtility.GetControlID(FocusType.Passive);
+        Event e = Event.current;
+
+        var mousePosition = Event.current.mousePosition * EditorGUIUtility.pixelsPerPoint;
+        mousePosition.y = Camera.current.pixelHeight - mousePosition.y;
+        Ray ray = Camera.current.ScreenPointToRay(mousePosition);
+
+        if (Event.current.type == EventType.MouseDown)
+        {
+            GUIUtility.hotControl = crtID;
+            e.Use();
+            grid = (Grid)target;
+            Vector3 createPos = new Vector3(Mathf.Floor(ray.origin.x / grid.width) * grid.width + grid.width / 2f,
+             Mathf.Floor(ray.origin.y / grid.height) * grid.height + grid.height / 2f, 0f);
+            if (CheckCompareObject(createPos))
+            {
+                GameObject createObject = (GameObject)PrefabUtility.InstantiatePrefab(grid.prefabsList);
+                createObject.transform.parent = grid.transform;
+                createObject.transform.position = createPos;
+                _objList.Add(createObject);
+            }
+        }
+    }
+
+    bool CheckCompareObject(Vector3 newPos)
+    {
+        foreach (var obj in _objList)
+        {
+            if (obj.transform.position == newPos)
+            {
+                _objList.Remove(obj);
+                DestroyImmediate(obj);
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Assets/JPER/Scripts/MapCreater/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public float width = 32f;
+    public float height = 32f;
+
+    public Color color = Color.white;
+
+    public GameObject prefabsList;
+
+    void OnDrawGizmos()
+    {
+        Vector3 pos = Camera.current.transform.position;
+        Gizmos.color = color;
+
+        for (float y = pos.y - 540f; y < pos.y + 540f; y += height)
+        {
+            Gizmos.DrawLine(new Vector3(10000000.0f, Mathf.Floor(y / height) * height, 0f),
+            new Vector3(-10000000, Mathf.Floor(y / height) * height, 0f));
+        }
+
+        for (float x = pos.x - 540f; x < pos.x + 540f; x += width)
+        {
+            Gizmos.DrawLine(new Vector3(Mathf.Floor(x / width) * width, 10000000, 0f),
+            new Vector3(Mathf.Floor(x / width) * width, -10000000, 0f));
+        }
+    }
+
+    public void DestoryObj(GameObject Destroy)
+    {
+        DestroyImmediate(Destroy);
+    }
+}
