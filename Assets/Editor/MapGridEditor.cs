1fae83f0ba4adbbfdec3c11b1c40e02d6ce674d9
Junwe <junwe99@gmail.com>
2020-03-16 12:29:51 +0900

    맵 제작툴 기능 추가

--- Assets/Editor/MapGridEditor.cs
+++ Assets/Editor/MapGridEditor.cs
@@ -22,6 +22,10 @@ public class MapGridEditor : Editor
         mousePosition.y = Camera.current.pixelHeight - mousePosition.y;
         Ray ray = Camera.current.ScreenPointToRay(mousePosition);
 
+        Handles.BeginGUI();
+        DrawPainter();
+        Handles.EndGUI();
+
         if (Event.current.button == 0)
         {
             if (grid.SelectIndex == 0)
@@ -77,6 +81,25 @@ public class MapGridEditor : Editor
         }
     }
 
+    private void DrawPainter()
+    {
+        grid = (Grid)target;
+        string[] ObjectOptions = new string[grid.prefabsList.Length];
+        string[] selectOptions = { "그리기", "지우기" };
+
+        for (int i = 0; i < ObjectOptions.Length; ++i)
+        {
+            if (grid.prefabsList[i] != null)
+                ObjectOptions[i] = grid.prefabsList[i].name;
+        }
+        grid.ObjectIndex = EditorGUILayout.Popup(grid.ObjectIndex, ObjectOptions, GUILayout.Width(80f));
+        grid.SelectIndex = EditorGUILayout.Popup(grid.SelectIndex, selectOptions, GUILayout.Width(80f));
+        if (GUILayout.Button("모두 삭제", GUILayout.Width(80f)))
+        {
+            AllObjectDelete();
+        }
+    }
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -95,6 +118,16 @@ public class MapGridEditor : Editor
 
         EditorGUILayout.EndHorizontal();
 
+    }
+
+    void AllObjectDelete()
+    {
+        Transform[] girdobjList = grid.parent.GetComponentsInChildren<Transform>();
+        foreach (var obj in girdobjList)
+        {
+            if (obj != null)
+                Undo.DestroyObjectImmediate(obj.gameObject);
+        }
     }
 
     bool CheckCompareObject(Vector3 newPos, bool destoryObject)
---
+++ Assets/Editor/MovingTrapEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(MovingTrap))]
+public class MovingTrapEditor : Editor
+{
+    MovingTrap targetObject;
+    void OnSceneGUI()
+    {
+        targetObject = (MovingTrap)target;
+
+        for (int i = 0; i < targetObject.points.Count; i++)
+        {
+            targetObject.points[i] = Handles.PositionHandle(targetObject.points[i], Quaternion.identity);
+        }
+
+        Handles.DrawAAPolyLine(targetObject.points.ToArray());
+
+        Handles.BeginGUI();
+        if (GUILayout.Button("포인트 추가", GUILayout.Width(80f)))
+        {
+            Undo.RecordObject(targetObject, "add points");
+            targetObject.points.Add(targetObject.transform.position);
+        }
+        if (GUILayout.Button("포인트 없애기", GUILayout.Width(80f)))
+        {
+            if (targetObject.points.Count > 0)
+            {
+                Undo.RecordObject(targetObject, "remove points");
+                targetObject.points.RemoveAt(targetObject.points.Count - 1);
+            }
+        }
+        Handles.EndGUI();
+    }
+}
