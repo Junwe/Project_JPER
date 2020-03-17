08e1efef22fe70841efccb09b1c2c2fd016f167b
sickhye0929 <chjh0929@gmail.com>
2020-03-18 00:13:52 +0900

    움직이는 발판 로직 추가. 콘솔프로 에셋 추가. 스테이지 추가 (미완성)

--- Assets/JPER/Scripts/MapCreater/MovingTrap.cs
+++ Assets/JPER/Scripts/MapCreater/MovingTrap.cs
@@ -5,4 +5,54 @@ using UnityEngine;
 public class MovingTrap : MonoBehaviour
 {
     public List<Vector3> points;
+
+    [SerializeField]
+    private float movingTime;
+    [SerializeField]
+    private float waitTime;
+    [SerializeField]
+    private bool isMoveWithPlayer;
+
+    public void StartMoving()
+    {
+        StartCoroutine(Moving());
+    }
+
+    public void StopMoving()
+    {
+        StopCoroutine(Moving());
+    }
+
+    private void Start()
+    {
+        StartMoving();
+    }
+
+    private IEnumerator Moving()
+    {
+        float elapsedTime = 0;
+        int direction = 1; // 1 또는 -1 만 사용.
+        var trapTransform = transform;
+        var waitSec = new WaitForSeconds(waitTime);
+
+        while (true)
+        {
+            elapsedTime += Time.deltaTime * direction;
+            if (elapsedTime >= movingTime || elapsedTime <= 0)
+            {
+                elapsedTime = elapsedTime >= movingTime ? movingTime : 0;
+                direction *= -1;
+                yield return waitSec;
+            }
+
+            float percent = elapsedTime / movingTime;
+            float temp = percent * (points.Count - 1);
+            int fromIndex = Mathf.FloorToInt(temp);
+            int toIndex = Mathf.CeilToInt(temp);
+            float alpha = fromIndex == 0 ? temp : temp % fromIndex;
+            trapTransform.position = Vector3.Lerp(points[fromIndex], points[toIndex], alpha);
+
+            yield return null;
+        }
+    }
 }
