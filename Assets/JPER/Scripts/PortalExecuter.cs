8588ecf83fd285b1b9b59382cb6b94361177854f
sickhye0929 <chjh0929@gmail.com>
2020-03-18 22:41:20 +0900

    포탈 진입 시 점프버튼 텍스트 업데이트 기능 추가.(임시).

--- Assets/JPER/Scripts/PortalExecuter.cs
+++ Assets/JPER/Scripts/PortalExecuter.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortalExecuter : MonoBehaviour
 {
+    [SerializeField]
+    private Text jumpText;
+
+    private string prevButtonText;
+
     public AbstPortal CurrentPortal { get; private set; }
 
     public void EntryPortal(AbstPortal newPortal)
     {
+        prevButtonText = jumpText.text;
+        jumpText.text = "포탈 사용";
         CurrentPortal = newPortal;
     }
 
     public void ExitPortal()
     {
+        jumpText.text = prevButtonText;
         CurrentPortal = null;
     }
 
