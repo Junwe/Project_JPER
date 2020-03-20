faf742cb46380e0db5f824ad31823ae340873a95
sickhye0929 <chjh0929@gmail.com>
2020-03-20 23:21:00 +0900

    플레이어 넉백 중 횡이동 중단되는 오류 수정.

--- Assets/JPER/Scripts/Player/PlayerMove.cs
+++ Assets/JPER/Scripts/Player/PlayerMove.cs
@@ -70,10 +70,6 @@ public class PlayerMove : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        /*Gizmos.matrix = Matrix4x4.TRS(overlapBoxTransform.position,
-                                      Quaternion.identity,
-                                      overlapBoxTransform.localScale);
-        Gizmos.color = Color.red;*/
         Gizmos.DrawCube(overlapBoxTransform.position, overlapBoxTransform.localScale);
     }
 #endif
@@ -100,9 +96,10 @@ public class PlayerMove : MonoBehaviour
                                               overlapBoxTransform.localScale,
                                               0.0f,
                                               layermask);
+
         if (col != null)
         {
-            if (_jumpInfomation.JumpState == 2 && overlapBoxTransform.transform.position.y >= col.transform.position.y + 0.3f)
+            if (_jumpInfomation.JumpState == 2 && overlapBoxTransform.transform.position.y >= col.transform.position.y + (col.bounds.size.y * 0.5f)) //0.3f)
             {
                 _jumpInfomation.BaseY = col.transform.position.y + 0.4f;
                 _playerPosition.PosY = col.transform.position.y + 0.4f;
@@ -157,23 +154,18 @@ public class PlayerMove : MonoBehaviour
 
     public void KnockbackProcess()
     {
-        _playerPosition.PosX += (knockbackInfomation.horizontalAcceleration - KnockbackInfomation.FRACTION) * knockbackInfomation.direction;
+        _playerPosition.PosX += knockbackInfomation.horizontalAcceleration * knockbackInfomation.direction;
         _playerPosition.PosY += knockbackInfomation.verticalAcceleration;
         knockbackInfomation.verticalAcceleration -= KnockbackInfomation.GRAVITY; // 올라가는 속도 점점 느려지게 수직 가속도 감소.
 
-        if (knockbackInfomation.isUp == true)
+        if (knockbackInfomation.isUp == true && knockbackInfomation.verticalAcceleration <= 0.0f)
         {
-            if (knockbackInfomation.verticalAcceleration <= 0.0f)
-                knockbackInfomation.isUp = false;
+            knockbackInfomation.isUp = false;
+            _jumpInfomation.JumpState = 2;
         }
-        else
-        {
-            if (_playerPosition.PosY <= knockbackInfomation.groundY)
-            {
-                _playerPosition.PosY = knockbackInfomation.groundY;
-                KnuckbackFlag = false;
-            }
-        }
+
+        if (_jumpInfomation.JumpState == 0)
+            KnuckbackFlag = false;
     }
 
     public void OnKnockback(int direction, float verticalAcceleration = KnockbackInfomation.DEFAULT_VERTICAL_SPEED, float horizontalAcceleration = KnockbackInfomation.DEFAULT_HORIZONTAL_SPEED)
@@ -183,9 +175,10 @@ public class PlayerMove : MonoBehaviour
             knockbackInfomation.direction = direction;
             knockbackInfomation.verticalAcceleration = verticalAcceleration;
             knockbackInfomation.horizontalAcceleration = horizontalAcceleration;
-            knockbackInfomation.groundY = _playerPosition.PosY;
             knockbackInfomation.isUp = true;
             KnuckbackFlag = true;
+
+            _jumpInfomation.JumpState = 1;
         }
     }
 }
