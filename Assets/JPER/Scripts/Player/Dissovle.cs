4e11f9ae1fe589d3fd612ada6023745966e3fc2c
Junwe <junwe99@gmail.com>
2020-04-01 01:28:04 +0900

    유니티 2019 업데이트, dissovle 셰이더 추가

---
+++ Assets/JPER/Scripts/Player/Dissovle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissovle : MonoBehaviour
+{
+    private Material _material;
+
+    private float _fade = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(StartDissovle());
+        }
+    }
+
+    IEnumerator StartDissovle()
+    {
+        while (_fade >= 0f)
+        {
+            _fade -= Time.deltaTime;
+
+            _material.SetFloat("_fade", _fade);
+
+            yield return new WaitForEndOfFrame();
+        }
+        _fade = 1f;
+
+        _material.SetFloat("_fade", _fade);
+    }
+}
