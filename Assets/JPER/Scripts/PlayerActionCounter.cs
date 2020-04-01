f9acf8941b5a8ac74af9e25a27b6c56eff12294d
sickhye0929 <chjh0929@gmail.com>
2020-04-02 00:00:10 +0900

    게임 클리어 결과 UI 레이아웃 작업.

--- Assets/JPER/Scripts/PlayerActionCounter.cs
+++ Assets/JPER/Scripts/PlayerActionCounter.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerActionCounter : MonoBehaviour
 {
+    public float PlayTime { get; private set; } = 0;
     public int FallCount { get; private set; } = 0;
     public int JumpCount { get; private set; } = 0;
     public float MovedDistance { get; private set; } = 0;
@@ -21,5 +22,24 @@ public class PlayerActionCounter : MonoBehaviour
     public void IncreasMovedDistance(float distancePerFrame)
     {
         MovedDistance += distancePerFrame;
+    }
+
+    public void StopPlayTimer()
+    {
+        StopCoroutine(PlayTimer());
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PlayTimer());
+    }
+
+    private IEnumerator PlayTimer()
+    {
+        while (true)
+        {
+            PlayTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
